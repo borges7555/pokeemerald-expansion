0494b10b0c966b339f9d975f4cd274f8e7e22cc5
Diogo Borges <diogomnborges@ua.pt>
2026-05-09 18:49:37 +0100

    edited trainers until 4th gym and made hm forgettable

--- src/caps.c
+++ src/caps.c
@@ -12,7 +12,7 @@ u32 GetCurrentLevelCap(void)
         {FLAG_BADGE01_GET, 15},
         {FLAG_BADGE02_GET, 22},
         {FLAG_BADGE03_GET, 29},
-        {FLAG_BADGE04_GET, 35},
+        {FLAG_BADGE04_GET, 39},
         {FLAG_BADGE05_GET, 40},
         {FLAG_BADGE06_GET, 48},
         {FLAG_BADGE07_GET, 56},
