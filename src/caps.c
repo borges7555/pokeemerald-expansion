39f058c3f34c9668c58c07550ffd999c32e08e61
Diogo Borges <diogomnborges@ua.pt>
2026-05-10 16:27:36 +0100

    edited trainers until 7th gym

--- src/caps.c
+++ src/caps.c
@@ -15,7 +15,7 @@ u32 GetCurrentLevelCap(void)
         {FLAG_BADGE04_GET, 39},
         {FLAG_BADGE05_GET, 45},
         {FLAG_BADGE06_GET, 53},
-        {FLAG_BADGE07_GET, 56},
+        {FLAG_BADGE07_GET, 62},
         {FLAG_BADGE08_GET, 65},
         {FLAG_IS_CHAMPION, 79},
     };
